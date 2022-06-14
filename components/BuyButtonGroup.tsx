d665126ecca975920575605fb342bdc21a276db5
Yang Liu <yangliu35@outlook.com>
2022-06-14 15:16:12 +0800

    feat: style

--- components/BuyButtonGroup.tsx
+++ components/BuyButtonGroup.tsx
@@ -22,12 +22,31 @@ function BuyButtonGroup(props: { id: string }) {
         });
     }, [addItem, id, name, price]);
 
-    if (!quantity) return <button onClick={handleAddItem}>Add to cart</button>;
     return (
-        <div>
-            <button onClick={() => incrementItem(id)}>+</button>
-            <span style={{ margin: "0 8px" }}>{quantity}</span>
-            <button onClick={() => decrementItem(id)}>-</button>
+        <div
+            style={{
+                width: 90,
+            }}
+        >
+            {quantity ? (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        border: "1px solid black",
+                        padding: 2,
+                        borderRadius: "4px",
+                    }}
+                >
+                    <button onClick={() => incrementItem(id)}>+</button>
+                    <span style={{ margin: "0 8px" }}>{quantity}</span>
+                    <button onClick={() => decrementItem(id)}>-</button>
+                </div>
+            ) : (
+                <button style={{ width: "100%" }} onClick={handleAddItem}>
+                    Add to cart
+                </button>
+            )}
         </div>
     );
 }
